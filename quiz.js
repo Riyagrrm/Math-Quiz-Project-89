93203a7cd0f40b74a1c225513ac6f5229313beb7
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2023-06-05 17:27:30 +1000

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,8 +1,20 @@
+player1_name=localStorage.getItem("player1Name")
+player2_name=localStorage.getItem("player2Name")
+player1_score=0
+player2_score=0
+question_turn="player1"
+answer_turn="player2"
+ans=0
+document.getElementById("player_question").innerHTML="Question turn - "+ player1_name
+document.getElementById("player_answer").innerHTML="Answer turn - "+ player2_name
+document.getElementById("player1_score").innerHTML="Score 1- "+ player1_score
+document.getElementById("player2_score").innerHTML="Score 2- "+ player2_score
 function send()
 {
     num1=document.getElementById("number1").value
     num2=document.getElementById("number2").value
-    ans=parseInt("num1")*parseInt("num2")
+    ans=parseInt(num1)*parseInt(num2)
+    console.log(ans)
     question_num="<h4>"+num1+"X"+num2+"</h4>";
     input_box="<br> Answer : <input id='input_check_box'>"
     check_btn="<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
@@ -14,19 +26,22 @@ function send()
 function check()
 {
     get_answer=document.getElementById("input_check_box").value
-    if (get_answer==actual_answer)
+    console.log(get_answer)
+    console.log(ans)
+    if (get_answer==ans)
     {
         if(answer_turn=="player1")
         {
-            update_player1_score = player1_score+1
-            document.getElementById("player1_score").innerHTML = update_player1_score
+            player1_score=player1_score+1
+            document.getElementById("player1_score").innerHTML= "Score 1- "+ player1_score 
         }
         else
         {
-            update_player2_score = player2_score+1
-            document.getElementById("player2_score").innerHTML = update_player2_score
+          player2_score=player2_score+1
+            document.getElementById("player2_score").innerHTML = "Score 2- "+ player2_score
         }
-        if (question_turn=="player1")
+    }
+    if (question_turn=="player1")
     {
         question_turn="player2"
         document.getElementById("player_question").innerHTML="Question turn - "+ player2_name
@@ -36,5 +51,14 @@ function check()
         question_turn="player1"
         document.getElementById("player_question").innerHTML="Question turn - "+player1_name
     }
+    if (answer_turn=="player1")
+    {
+        answer_turn="player2"
+        document.getElementById("player_answer").innerHTML="Answer turn - "+ player2_name
+    }
+    else
+    {
+        answer_turn="player1"
+        document.getElementById("player_answer").innerHTML="Answer turn - "+player1_name
     }
 }
